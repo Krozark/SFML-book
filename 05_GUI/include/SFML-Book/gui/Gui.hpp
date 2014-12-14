84dc75ba9fa1c9ff76da09b30f4fd3223d45570d
Krozark <maxime.barbier1991@gmail.com>
2014-12-14 19:36:56 +0100

    add global include file for gui

---
+++ 05_GUI/include/SFML-Book/gui/Gui.hpp
@@ -0,0 +1,5 @@
+#include <SFML-Book/gui/Button.hpp>
+#include <SFML-Book/gui/Frame.hpp>
+#include <SFML-Book/gui/Layout.hpp>
+#include <SFML-Book/gui/VLayout.hpp>
+#include <SFML-Book/gui/Widget.hpp>
