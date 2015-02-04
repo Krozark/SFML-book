ffbfe245c3246142cbdb8f18aa560c1f0da37075
Krozark <maxime.barbier1991@gmail.com>
2015-02-04 23:54:23 +0100

    fix miltipl spawn

--- 07_2D_iso_game/src/SFML-Book/Level.cpp
+++ 07_2D_iso_game/src/SFML-Book/Level.cpp
@@ -48,8 +48,8 @@ namespace book
         
         //ES
         systems.add<SysAIMain>();
-        //systems.add<SysAIWarrior>(*this);
-        //systems.add<SysAIDefender>(*this);
+        systems.add<SysAIWarrior>(*this);
+        systems.add<SysAIDefender>(*this);
         systems.add<SysAISpawner>(*this);
         systems.add<SysAIWalker>(*this);
         systems.add<SysAIFlyer>(*this);
