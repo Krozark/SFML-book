ad0e605c5e88834b63ea7946412c2194ac7b002d
Krozark <maxime.barbier1991@gmail.com>
2015-02-18 21:11:28 +0100

    remove file duplication

--- 08_networking/src/SFML-Book/common/main.cpp
+++ 08_networking/src/SFML-Book/common/main.cpp
@@ -1,19 +1,8 @@
-#include <SFML-utils/Map.hpp>
-
-#include <SFML-Book/common/random.hpp>
-#include <iostream>
-
-//#include <SFML-Book/common/Configuration.hpp>
-//#include <SFML-Book/common/Game.hpp>
+#include <SFML-Book/common/Configuration.hpp>
 
 int main(int argc,char* argv[])
 {
-    book::rand_init();
-    /*book::Configuration::initialize();
-
-    book::Game game(1600,900);
-    game.load("./media/map.json");
-    game.run();*/
+    book::Configuration::initialize();
 
     return 0;
 };
