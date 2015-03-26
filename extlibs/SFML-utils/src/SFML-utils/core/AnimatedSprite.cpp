f521e8d484917ed6e410f532f57c595b9413f5ad
Krozark <maxime.barbier1991@gmail.com>
2015-03-26 19:29:58 +0100

    add asert

--- extlibs/SFML-utils/src/SFML-utils/core/AnimatedSprite.cpp
+++ extlibs/SFML-utils/src/SFML-utils/core/AnimatedSprite.cpp
@@ -1,5 +1,6 @@
 #include <SFML-utils/core/AnimatedSprite.hpp>
 #include <SFML-utils/core/Animation.hpp>
+#include <cassert>
 
 namespace sfutils
 {
@@ -78,11 +79,9 @@ namespace sfutils
 
     void AnimatedSprite::setFrame(size_t index)
     {
-        if(_animation)
-        {
-            _currentFrame = index % _animation->size();
-            setFrame(_currentFrame,true);
-        }
+        assert(_animation);
+        _currentFrame = index % _animation->size();
+        setFrame(_currentFrame,true);
     }
 
     void AnimatedSprite::setColor(const sf::Color& color)
